cd995731aed2362887b808dba887645587a4fa1e
dleyvacastro <dleyvacastro@gmail.com>
2021-12-05 21:01:26 -0500

    limpieza de archivo

--- config.py
+++ config.py
@@ -1,30 +1,17 @@
-from typing import List  # noqa: F401
-import os
-import subprocess
+# Qtile imports
 from libqtile import bar, layout, widget, hook, qtile
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
 from libqtile.utils import guess_terminal
-from themes import material_ocean as colors
 from libqtile.log_utils import logger
-
-# with open("material-ocean.json", 'r') as f:
-#    colors = json.load(f)
-#    print(colors)
-
-mod = "mod4"
-browser = 'brave-browser'
-terminal = "konsole -e fish"
-font = "UbuntuMono Nerd Font"
-
-
-def test_funct(qtile):
-
-    try:
-        g1 = list(qtile.cmd_groups())
-        logger.warning(f'Type of returning: {g1}')
-    except:
-        logger.warning('exploto')
+# Standard Imports
+from typing import List  # noqa: F401
+import os
+import subprocess
+# Config Imports
+from themes import material_ocean as colors
+from settings.default_globals import *
+from settings.keys import keys
 
 
 @hook.subscribe.startup_once
@@ -114,93 +101,6 @@ def MusicPayer(fb='text', bg='dark'):
     ]
 
 
-def open_pages(pages: list, browser='brave-browser'):
-    return f'{i + " " for i in pages}'
-
-
-keys = [
-    Key([mod, "shift"], "p", lazy.function(test_funct(qtile))),
-    # Switch between windows
-    Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
-    Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
-    Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
-    Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
-    Key([mod], "c", lazy.layout.next(),
-        desc="Move window focus to other window"),
-
-    # Move windows between left/right columns or move up/down in current stack.
-    # Moving out of range in Columns layout will create new column.
-    Key([mod, "shift"], "h", lazy.layout.shuffle_left(),
-        desc="Move window to the left"),
-    Key([mod, "shift"], "l", lazy.layout.shuffle_right(),
-        desc="Move window to the right"),
-    Key([mod, "shift"], "j", lazy.layout.shuffle_down(),
-        desc="Move window down"),
-    Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
-
-    # Grow windows. If current window is on the edge of screen and direction
-    # will be to screen edge - window would shrink.
-    Key([mod, "control"], "h", lazy.layout.grow_left(),
-        desc="Grow window to the left"),
-    Key([mod, "control"], "l", lazy.layout.grow_right(),
-        desc="Grow window to the right"),
-    Key([mod, "control"], "j", lazy.layout.grow_down(),
-        desc="Grow window down"),
-    Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
-    Key([mod, "shift"], "f", lazy.window.toggle_floating()),
-
-    # Toggle between split and unsplit sides of stack.
-
-    # Unsplit = 1 window displayed, like Max layout, but still with
-    # multiple stack panes
-    Key([mod, "shift"], "Return", lazy.layout.toggle_split(),
-        desc="Toggle between split and unsplit sides of stack"),
-    Key(["mod1", "control"], "t", lazy.spawn(
-        terminal), desc="Launch terminal"),
-
-    # Toggle between different layouts as defined below
-    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
-    Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
-    Key([mod], "o", lazy.layout.maximize()),
-    Key([mod, "shift"], "space", lazy.layout.flip()),
-    Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
-    Key([mod], "m", lazy.window.toggle_fullscreen()),
-
-    Key([mod, "mod1"], "r", lazy.restart(), desc="Restart Qtile"),
-    Key([mod, "mod1"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
-    Key([mod], "space", lazy.spawn('rofi -show drun'),
-        desc="Spawn a command using a prompt widget"),
-    Key(["control"], "space", lazy.spawn('rofi -show')),
-
-    Key([mod, "mod1"], "j",
-        lazy.to_screen(0)),
-
-    Key([mod, "mod1"], "k",
-        lazy.to_screen(1)),
-    Key([mod], "b", lazy.spawn('brave-browser')),
-    # Media
-    Key([], "XF86AudioRaiseVolume", lazy.spawn('amixer -q set Master 5%+')),
-    Key([], "XF86AudioLowerVolume", lazy.spawn('amixer -q set Master 5%-')),
-    Key([], 'XF86AudioPlay', lazy.spawn(
-        'dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause'
-    )),
-    Key([], 'XF86AudioNext', lazy.spawn(
-        'dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next'
-    )),
-    Key([], 'XF86AudioPrev', lazy.spawn(
-        'dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Previous'
-    )),
-    # ScreenShot
-    Key([], "Print", lazy.spawn(
-        "scrot '/tmp/%F_%T_$wx$h.png' -e 'xclip -selection clipboard -target image/png -i $f'"), desc="Full ScreenShot"),
-    Key(["shift", mod], "s", lazy.spawn(
-        "scrot -s '/tmp/%F_%T_$wx$h.png' -e 'xclip -selection clipboard -target image/png -i $f && rm $f'"), desc="Selected ScreenShot"),
-    Key([mod], "v", lazy.spawn('/usr/bin/diodon'), desc="Calls diodon"),
-]
-
-# groups = [Group(i) for i in [
-#    "   ", "   ", "  ", "   ", "  ", "   ", "   ", "   ", "   ",
-# ]]
 g = [
     # Group("   "),
     Group("   ", Match(wm_class=["Firefox", "Brave-browser"])),
@@ -217,17 +117,6 @@ groups = g + [Group(i) for i in [
     "7", "8", "9",
 ]]
 
-keys.extend(
-    [
-        Key([mod, "shift"], "e", lazy.spawn(
-            'brave-browser --new-window https://e-aulas.urosario.edu.co/ lofi.cafe https://es.symbolab.com/')),
-        Key([mod, "shift"], "a", lazy.spawn(
-            'brave-browser --new-window https://www3.animeflv.net/ https://www.mangatigre.com/ https://manganyaa.com/es https://www.youtube.com/'))
-
-    ]
-)
-
-
 for i, group in enumerate(groups):
     actual_key = str(i + 1)
     keys.extend([
@@ -236,23 +125,6 @@ for i, group in enumerate(groups):
         # Send window to workspace N
         Key([mod, "shift"], actual_key, lazy.window.togroup(group.name))
     ])
-# groups = [Group(i) for i in "123456789"]
-#
-# for i in groups:
-#    keys.extend([
-#        # mod1 + letter of group = switch to group
-#        Key([mod], i.name, lazy.group[i.name].toscreen(),
-#            desc="Switch to group {}".format(i.name)),
-#
-#        # mod1 + shift + letter of group = switch to & move focused window to group
-#        # Key([mod, "shift"], i.name, lazy.window.togroup(i.name, switch_group=True),
-#        #    desc="Switch to & move focused window to group {}".format(i.name)),
-#        # Or, use below if you prefer not to switch to that group.
-#        # # mod1 + shift + letter of group = move focused window to group
-#        Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
-#            desc="move focused window to group {}".format(i.name)),
-#    ])
-
 layout_conf = {
     'border_focus': colors['focus'][0],
     'border_width': 1,
@@ -264,9 +136,9 @@ layouts = [
     layout.Max(),
     # Try more layouts by unleashing below layouts.
     # layout.Stack(num_stacks=2),
-    layout.RatioTile(**layout_conf),
     layout.MonadTall(**layout_conf),
     layout.MonadWide(**layout_conf),
+    layout.RatioTile(**layout_conf),
     # layout.Bsp(**layout_conf),
     # layout.Matrix(**layout_conf),
     # layout.Tile(),
